09f520189d009e163796a2076e413ffc0760be95
Piotr Szuta <p.szuta@alfaelektro.pl>
2020-02-29 19:11:40 +0100

    Zadania rekrutacyjne dla ANSTA

---
+++ zadanie1.py
@@ -0,0 +1,13 @@
+﻿# ZADANIE 1.
+# GENERATOR KODÓW POCZTOWYCH
+# przyjmuje 2 stringi: "79-900" i "80-155" i zwraca listę kodów pomiędzy nimi
+
+
+def postal_code_generator(start: str, stop: str) -> list:
+    start = int(start.replace('-', ''))
+    stop = int(stop.replace('-', ''))
+    postal_code_list = [f'{str(x)[:2]}-{str(x)[2:]}' for x in range(start, stop + 1)]
+    return postal_code_list
+
+
+
---
+++ zadanie3.py
@@ -0,0 +1,13 @@
+﻿# ZADANIE 3.
+# NALEŻY WYGENEROWAĆ LISTĘ LICZB OD 2 DO 5.5 ZE SKOKIEM CO 0.5,
+# DANE WYNIKOWE MUSZĄ BYĆ TYPU DECIMAL.
+
+
+def number_generator(start: float = 2, stop: float = 5.5) -> list:
+    start = int(start * 10)
+    stop = int(stop * 10)
+    number_list = [x / 10 for x in range(start, stop + 1, 5)]
+    return number_list
+
+
+
